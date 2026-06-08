7730179366862674dc0d95c361de209cc5d6966b
FavourLoverboy <favourehio2019@gmail.com>
2026-06-08 13:48:00 +0100

    fix: correct the spelling of count

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
                     className="counter"
                     onClick={() => setCount((count) => count + 1)}
                 >
-                    Counts is {count}
+                    Count is {count}
                 </button>
             </section>
 

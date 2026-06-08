53485f84f4c88e61d9b474d7793c176f27ecc52d
FavourLoverboy <favourehio2019@gmail.com>
2026-06-08 14:14:13 +0100

    feat: add commitlint

--- commitlint.config.js
+++
@@ -1,6 +0,0 @@
-export default { 
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'refactory', 'perf', 'test', ]],
-    }
-};
